c547ea90700dd89b82df3ad8ea4ab2a56b8231bb
ooma tell <otymitu@yahoo.com>
2021-10-04 00:18:57 -0400

    new object for sale users

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -9,6 +10,12 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.use(bodyParser.json());  
+app.post('/api/stuff', (req, res, next) => {
+console.log(req.body);
+  });
+
 
   
 app.use('/api/stuff', (req, res, next) => {
